5a22ddacbb1059827cf3c64d4daf03b3b5e704a0
Gamze Yazicioglu <gamze.ayaz.95@hotmail.com>
2024-01-21 21:03:57 +0300

    Lesson02_Part01

---
+++ src/test/java/Lesson02_matematikselIslemler_ifElseStatements/C01_MatematikselIslemler.java
@@ -0,0 +1,15 @@
+package Lesson02_matematikselIslemler_ifElseStatements;
+
+public class C01_MatematikselIslemler {
+
+    public static void main(String[] args) {
+        int sayi1 = 10;
+        int sayi2 = 20;
+        String str =  "Java";
+
+        System.out.println(str + sayi1 + sayi2); // Java1020
+
+        System.out.println(str + (sayi1 + sayi2)); // Java30
+    }
+
+}
